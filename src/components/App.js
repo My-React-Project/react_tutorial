8c6abc8b0937ac7cb1427b72854e3c7a36617be9
choijaewoong <jwchoi0129@gmail.com>
2017-02-23 20:30:08 +0900

    list에 데이터 추가 기능 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import update from 'react-addons-update';
 
 class App extends React.Component {
   render() {
@@ -21,6 +22,25 @@ class Contacts extends React.Component {
     };
   }
 
+  /* 데이터 추가
+    this.setState({
+      list: update(
+        this.state.list,
+        {
+          $push: [newObj, newObj2]
+        }
+      )
+    });
+  */
+  addContact(name, phone) {
+    let newContact = update(this.state, {
+      contactData: {
+        $push: [{"name": name, "phone": phone}]
+      }
+    });
+    this.setState(newContact);
+  }
+
   render() {
     return (
       <div>
@@ -33,6 +53,7 @@ class Contacts extends React.Component {
 
           })}
         </ul>
+        <ContactCreator onInsert={this.addContact.bind(this)}/>
       </div>
     );
   }
@@ -43,6 +64,43 @@ class ContactInfo extends React.Component {
     return(
       <li>{this.props.name} {this.props.phone}</li>
     );
+  }
+}
+
+class ContactCreator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      phone: ""
+    }
+  }
+  handleChange(e) {
+    var nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
+  handleClick() {
+    this.props.onInsert(this.state.name, this.state.phone);
+    this.setState({
+      name: "",
+      phone: ""
+    });
+  }
+  render() {
+    return(
+      <div>
+        <p>
+          <input type="text" name="name" placeholder="name"
+                 value={this.state.name} onChange={this.handleChange.bind(this)}/>
+          <input type="text" name="phone" placeholder="phone"
+                 value={this.state.phone} onChange={this.handleChange.bind(this)}/>
+           <button onClick={this.handleClick.bind(this)}>
+             Insert
+          </button>
+        </p>
+      </div>
+    )
   }
 }
 
