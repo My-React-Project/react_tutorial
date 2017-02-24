8dd09cd506967b22c4a65e62ea86c59f2b3d869c
choijaewoong <jwchoi0129@gmail.com>
2017-02-24 15:19:44 +0900

    ref 사용 예제 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,99 +1,37 @@
 import React from 'react';
-import update from 'react-addons-update';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            number: 0,
-            show: false
-        }
-    }
 
-    _increase() {
-        this.setState({
-            number: this.state.number + 1
-        });
-    }
-
-    _show() {
-        this.setState({show: true});
-    }
-
-    _unmount() {
-        this.setState({number: 0, show: false});
-    }
-
-    render() {
-        let component = (<Card number={this.state.number}/>);
-        return (
-            <div>
-                <Button caption="Show Card" customClass="green" onClick={this._show.bind(this)}/>
-                <Button caption="Increase Number" customClass="blue" onClick={this._increase.bind(this)}/>
-                <Button caption="Unmount Card" customClass="red" onClick={this._unmount.bind(this)}/> {this.state.show
-                    ? component : ""}
-            </div>
-        );
-    }
-}
-
-class Button extends React.Component {
-    render() {
-        let className = "ui button " + this.props.customClass;
-
-        return (
-            <div onClick={this.props.onClick} className={className}>{this.props.caption}</div>
-
-        )
-    }
-}
-
-class Card extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log("constructor");
-  }
-  componentWillMount() {
-    console.log("componentWillMount");
-  }
-  componentDidMount() {
-    console.log("componentDidMount");
-  }
-
-  componentWillReceiveProps(nextProps) {
-    console.log("componentWillReceiveProps: " + JSON.stringify(nextProps));
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    if(JSON.stringify(nextProps) != JSON.stringify(this.props)) {
-      console.log("shouldComponentUpdate: " + JSON.stringify(nextProps) + " " + JSON.stringify(nextState));
-      return true;
-    }
-    return false;
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    console.log("componentWillUpdate: " + JSON.stringify(nextProps) + " " + JSON.stringify(nextState));
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log("componentDidUpdate: " + JSON.stringify(prevProps) + " " + JSON.stringify(prevState));
-  }
-
-  componentWillUnmount() {
-    console.log("componentWillUnmount");
+  handleClick() {
+    this.input.value = "This is TextBox"
   }
 
   render() {
-    console.log("render");
     return (
-      <div className="ui card">
-        <div className="content">
-          number: {this.props.number}
-        </div>
+      <div>
+        {/* <TextBox ref={ref => this.textBox = ref}/> */}
+        <input ref={ref => this.input = ref}/>
+        <button onClick={this.handleClick.bind(this)}>Click Me</button>
+      </div>
+    );
+  }
+}
+
+class TextBox extends React.Component {
+  render() {
+    return (
+      <div>
+        {/*
+          {ref => this.input = ref}
+          =>
+          function(ref) {
+            return this.input = ref;
+          }
+        */}
+        <input ref={ref => this.input = ref}></input>
       </div>
     )
-  };
+  }
 }
 
 export default App;
